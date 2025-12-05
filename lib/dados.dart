1506444ef9519a89547f84b3bb8fe5fdf8c23807
Mateus Freitas <mateus.santosf05@gmail.com>
2025-12-04 23:08:19 -0300

    Subindo atualizações

---
+++ lib/dados.dart
@@ -0,0 +1,16 @@
+import 'pergunta_respostas.dart';
+
+final List<PerguntaRespostas> perguntasRespostas = [
+  PerguntaRespostas(
+    pergunta: 'Qual é a sua cor favorita?',
+    respostas: ['rosa','verde','amarelo','azul','vermelho','Cor de burro quando foge'],
+  ),
+  PerguntaRespostas(
+    pergunta: 'Qual o seu animal favorito?',
+    respostas: ['Gato','Cão','Ave','Porco','Ornitorrinco'],
+  ),
+  PerguntaRespostas(
+    pergunta: 'Qual é o seu time?',
+    respostas: ['Palmeiras','Corinthians','São Paulo','Santos','Arranca Toco F.C.'],
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './botoes.dart';
+import './dados.dart';
+import './lista_perguntas.dart';
+//import './resposta.dart';
+
 
 void main() {
   runApp(const PerguntasApp());
@@ -26,58 +29,50 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home>  {
 
-  final perguntas = [
-    {
-      'pergunta' : 'Qual a sua cor favorita?',
-      'respostas': ['rosa','verde','amarelo','azul','vermelho','Cor de burro quando foge'],
-    },
+  final dados = perguntasRespostas;
+  List respostas = [];
+  var indicePergunta = 0;
 
-    {
-      'pergunta' : 'Qual o seu animal favorito?',
-      'respostas': ['Gato','Cão','Ave','Porco','Ornitorrinco'],
-    },
-
-    {
-      'pergunta' : 'Qual é o seu time?',
-      'respostas': ['Palmeiras','Corinthians','São Paulo','Santos','Arranca Toco F.C.'],
-    },
-  ];
-
-  var indicePergunta = 2;
-
-  void responder() {
-    if (indicePergunta < perguntas.length - 1) {
-      indicePergunta++;
-    } else {
-      indicePergunta = 0;
+  void responder(String r) {
+    String p = dados[indicePergunta].pergunta;
+    respostas.add({'pergunta': p, 'resposta': r});
+    indicePergunta++;
+    setState(() {});
     }
-  }
+
+    void reiniciar() {
+      setState(() {
+        indicePergunta = 0;
+        respostas = [];
+      });
+    }
+
+    bool get temPergunta {
+      return indicePergunta < dados.length;
+    }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mateus Freitas', style: TextStyle(fontSize: 30)),
+        title: const Text(
+          'Parisotto',
+          style: TextStyle(fontSize: 30)
+        ), // Text
         centerTitle: true,
         backgroundColor: Colors.amber,
         toolbarHeight: 80,
-      ),
+      ), // AppBar
       body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            Text(
-              perguntas[indicePergunta]['pergunta'].toString(), 
-              style: TextStyle(fontSize: 25)
-            ),
-            SizedBox(height: 20,), 
-            ...((perguntas[indicePergunta]['respostas'] as List<String>)
-              .map((textoBotao) => Botoes(resp: responder, txt: textoBotao))
-              .toList()),
-          ],
-        ),
-      ),
-    );
+        padding: const EdgeInsets.all(20),
+        child: temPergunta
+          ? ListaPerguntas(
+              indicePergunta: indicePergunta,
+              perguntas: dados,
+              responder: responder,
+            ) // ListaPerguntas
+          : null //Resultado(respostas, reiniciar),
+      ), // Padding
+    ); // Scaffold
   }
-
 }
